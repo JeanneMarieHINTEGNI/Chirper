16633f5f4e062e14a8d6ec973d70de5775d25b2f
Morgane BINAZON <morganebinazon@gmail.com>
2024-12-09 18:45:55 +0100

    sixieme commit

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -1,12 +1,10 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
 {
-    protected $fillable = [
-        'message',
-    ];
+    protected $fillable = ['user_id', 'message', 'created_at', 'updated_at'];
 }
